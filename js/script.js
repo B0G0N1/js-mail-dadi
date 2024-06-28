49b3a42c436e3afb69b239e4810921dc30690572
B0G0N1 <michelebogoni2001@gmail.com>
2024-06-28 20:15:11 +0200

    [UPD] Update HTML and Javascript for Email

--- js/script.js
+++ js/script.js
@@ -1,19 +1,28 @@
 // ------------------------------
 //    CODICE ACCESSO CON EMAIL
 // ------------------------------
-const authorized_emails_list = ["pippo@gmail.com", "pluto@gmail.com", "tizio@gmail.com", "caio@gmail.com", "sempronio@gmail.com"];
-const user_email = prompt("Inserisci la tua email: ");
-let access_granted = false;
+function checkAccess() {
+    const authorized_emails_list = ["x", "pippo@gmail.com", "pluto@gmail.com", "tizio@gmail.com", "caio@gmail.com", "sempronio@gmail.com"];
+    const user_email = document.getElementById("input-email").value;
+    let access_granted = false;
 
-for (let i = 0; i < authorized_emails_list.length; i++) {
-    if (user_email === authorized_emails_list[i]) {
-        access_granted = true;
-        break;
+    for (let i = 0; i < authorized_emails_list.length; i++) {
+        if (user_email === authorized_emails_list[i]) {
+            access_granted = true;
+            break;
+        }
+    }
+
+    const accessBox = document.getElementById("access-box");
+    const accessMessage = document.getElementById("access-message");
+
+    if (access_granted) {
+        accessBox.className = 'access-display access-green';
+        accessMessage.textContent = "Access Consented";
+    } else {
+        accessBox.className = 'access-display access-red';
+        accessMessage.textContent = "Access Denied";
     }
 }
 
-if (access_granted) {
-    alert("ACCESSO CONSENTITO");
-} else {
-    alert("ACCESSO NEGATO");
-}
+document.getElementById("check-access").onclick = checkAccess;
